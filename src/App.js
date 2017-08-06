0a09a2b109de70e19d9ef35f050623f04470edc8
AgtLucas <lucas@agtlucas.com>
2017-08-06 16:31:17 +0200

    Update app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor () {
+    super()
+    this.state = { counter: 0 }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    this.setState(({ counter }) => ({
+      counter: counter + 1
+    }))
+  }
+
+  render () {
+    if (this.state.counter === 5) {
+      throw new Error('I crashed!')
+    }
+
+    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>
   }
 }
 
-export default App;
+export default App
